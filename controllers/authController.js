1ac2efa656c6544912dad40f0c750a60a4fbe225
Bahodir8586 <bahodira213@gmail.com>
2021-10-04 17:25:52 +0500

    created functions to create and send jwt tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,35 @@
+const jwt = require("jsonwebtoken");
+
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 
+const signToken = (id) => {
+  const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+  return token;
+};
+
+const createAndSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+  res.cookie("jwt", token, cookieOptions);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user
+    }
+  });
+};
 
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
@@ -15,4 +43,12 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError("Incorrect email or password"), 401);
   }
 //   Give access and send token to the user there
+});
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const { name, email, password, passwordConfirm } = req.body;
+  const newUser = await User.create({
+    name, email, password, passwordConfirm
+  });
+//  Give access and send token to the user there
 });
