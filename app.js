353202a676f6496ce0562761e0142f115d10ec91
Bahodir8586 <bahodira213@gmail.com>
2021-10-04 16:41:30 +0500

    rate limiter middleware

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const express=require('express')
+const express=require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,9 +11,17 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+// Limiting the rate of the requests
+const limiter = rateLimit({
+  max: 30,
+  windowMs: 60 * 1000,
+  message: 'Too many requests from this IP. Please try again in an hour'
+});
+app.use('/api', limiter);
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Routes will be there
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
