073289aa8104a6bd589740e788349a6aafd09d67
Bahodir8586 <bahodira213@gmail.com>
2021-10-04 16:39:00 +0500

    created global error handler middleware

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express=require('express')
+const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
+
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,71 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDb = err => {
+  const message = `Invalid  ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDb = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDb = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')} `;
+  return new AppError(message, 400);
+};
+
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired. Please log in again', 401);
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+const sendErrorProduction = (err, res) => {
+  // Operational, trusted errors: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+    // Programming, or unknown error: don't leak error details
+  } else {
+    //   1) Log the error
+    console.error('Error', err);
+    //  2) Send generic message
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went wrong'
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDb(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDb(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDb(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+    sendErrorProduction(error, res);
+  }
+}
