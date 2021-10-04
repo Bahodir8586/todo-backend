05d2820c8d158415ebad3aa785e5965f30942a57
Bahodir8586 <bahodira213@gmail.com>
2021-10-04 16:45:33 +0500

    hpp

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-const express=require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const xss = require('xss-clean');
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 // Middlewares against attacks
 const limiter = rateLimit({
   max: 30,
   windowMs: 60 * 1000,
-  message: 'Too many requests from this IP. Please try again in an hour'
+  message: "Too many requests from this IP. Please try again in an hour"
 });
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 app.use(xss());
+app.use(hpp({ whitelist:[""] }));
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Routes will be there
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
