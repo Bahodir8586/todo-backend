daada9920e5165706045c73d5f387cf6c32eafd7
Bahodir8586 <bahodira213@gmail.com>
2021-10-05 11:00:32 +0500

    routes implemeneted to CRUD of user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const authController=require('./../controllers/authController')
+const authController = require("./../controllers/authController");
+const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-router.post('/signup', authController.signup)
-router.post('/login', authController.login)
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
 
-router.patch('/updatePassword', authController.protect, authController.updatePassword)
+router.patch("/updatePassword", authController.protect, authController.updatePassword);
+router.route("/")
+  .patch(authController.protect, userController.update)
+  .delete(authController.protect, userController.delete);
+
 
 module.exports = router;
