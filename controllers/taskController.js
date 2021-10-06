9f3ebd074af64d94c63c841418c55bfe760b86ef
Bahodir8586 <bahodira213@gmail.com>
2021-10-06 13:45:33 +0500

    delete taks is done

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -30,6 +30,11 @@ exports.updateTask = catchAsync(async (req, res, next) => {
 
 });
 exports.deleteTask = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { $pull: { "tasks": { _id: req.params.id } } });
+  res.status(204).json({
+    status: "success",
+    data: null
+  });
 
 });
 exports.getTask = catchAsync(async (req, res, next) => {
