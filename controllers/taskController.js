500d352912eccba2efd3ce83f613d3bebd31f542
Bahodir8586 <bahodira213@gmail.com>
2021-10-06 12:46:46 +0500

    task controller initialized

---
+++ controllers/taskController.js
@@ -0,0 +1,15 @@
+const catchAsync=require('./../utils/catchAsync')
+const AppError=require('./../utils/appError')
+
+exports.getTasks=catchAsync(async(req,res,next)=>{
+
+})
+exports.createTask=catchAsync(async(req,res,next)=>{
+
+})
+exports.updateTask=catchAsync(async(req,res,next)=>{
+
+})
+exports.deleteTask=catchAsync(async(req,res,next)=>{
+
+})
