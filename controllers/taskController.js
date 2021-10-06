2075d42529bfcd342aad2e23f77211ec20dbb77a
Bahodir8586 <bahodira213@gmail.com>
2021-10-06 13:38:56 +0500

    get all tasks of user done

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -4,7 +4,13 @@ const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 
 exports.getTasks = catchAsync(async (req, res, next) => {
-
+  const tasks = await User.findById(req.user.id).select("tasks");
+  res.status(200).json({
+    status: "success",
+    data: {
+      tasks
+    }
+  });
 });
 exports.createTask = catchAsync(async (req, res, next) => {
   const newTask = req.body;
